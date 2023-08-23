4265b4aab0b12fd6dbbd9c2eec3a77a033ecef07
rhunt222 <richard.hunt2@gmail.com>
2023-08-23 14:24:31 -0700

    Update deepl.py
    ignore variables in curly brackets

--- json_translate/translators/deepl.py
+++ json_translate/translators/deepl.py
@@ -11,83 +11,94 @@ class DeepLTranslator(BaseTranslator):
     """DeepL translator class."""
 
     def translate_string(self, text: str) -> str:
-        """Translate a specifig string.
+        """Translate a specific string.
 
-        Test with curl:
-        $ curl https://api-free.deepl.com/v2/translate \
-            -d auth_key=YOUR-API-KEY-HERE \
-            -d "text=Hello, world!" \
-            -d "target_lang=ES"
+    Test with curl:
+    $ curl https://api-free.deepl.com/v2/translate \\
+        -d auth_key=YOUR-API-KEY-HERE \\
+        -d "text=Hello, world!" \\
+        -d "target_lang=ES"
 
-        :param text: string to translate
-        :return: string translation
-        """
-        if not isinstance(text, str):
-            return text
+    :param text: string to translate
+    :return: string translation
+    """
+    if not isinstance(text, str):
+        return text
 
-        cached_result = self.cached.get(text)
-        if cached_result:
-            self.log_translation(
-                input_text=text,
-                result=f"{cached_result} (cached)",
-            )
-            return cached_result
-
-        time.sleep(self.sleep)
-
-        data = {
-            "target_lang": self.target_locale,
-            "auth_key": os.environ.get("DEEPL_AUTH_KEY"),
-            "text": text,
-            "preserve_formatting": "1",
-        }
-
-        if self.source_locale is not None:
-            data["source_lang"] = self.source_locale
-
-        if self.glossary is not None:
-            data["glossary_id"] = self.glossary
-
-        data = parse.urlencode(data).encode()
-
-        req = request.Request(DEEPL_API_ENDPOINT, data=data)
-        response = request.urlopen(req)  # nosec
-
-        if response.status != 200:
-            self.log_translation(
-                input_text=text,
-                result=f"response status: {response.status}",
-                status=self.Status.error,
-            )
-            return text
-
-        response_data = json.loads(response.read())
-
-        if "translations" not in response_data:
-            self.log_translation(
-                input_text=text,
-                result=f"response empty: {response_data}",
-                status=self.Status.error,
-            )
-            return text
-
-        if len(response_data["translations"]) > 1:
-            self.log_translation(
-                input_text=text,
-                result=f"more than 1 translation: {response_data['translations']})",
-                status=self.Status.warning,
-            )
-
+    cached_result = self.cached.get(text)
+    if cached_result:
         self.log_translation(
             input_text=text,
-            result=response_data["translations"][0]["text"],
-            status=self.Status.success,
+            result=f"{cached_result} (cached)",
+        )
+        return cached_result
+
+    # Identify variables surrounded by curly brackets
+    variables = re.findall(r'\{.*?\}', text)
+    for i, var in enumerate(variables):
+        placeholder = f"PLACEHOLDER_{i}"
+        text = text.replace(var, placeholder)
+
+    time.sleep(self.sleep)
+
+    data = {
+        "target_lang": self.target_locale,
+        "auth_key": os.environ.get("DEEPL_AUTH_KEY"),
+        "text": text,
+        "preserve_formatting": "1",
+    }
+
+    if self.source_locale is not None:
+        data["source_lang"] = self.source_locale
+
+    if self.glossary is not None:
+        data["glossary_id"] = self.glossary
+
+    data = parse.urlencode(data).encode()
+
+    req = request.Request(DEEPL_API_ENDPOINT, data=data)
+    response = request.urlopen(req)  # nosec
+
+    if response.status != 200:
+        self.log_translation(
+            input_text=text,
+            result=f"response status: {response.status}",
+            status=self.Status.error,
+        )
+        return text
+
+    response_data = json.loads(response.read())
+
+    if "translations" not in response_data:
+        self.log_translation(
+            input_text=text,
+            result=f"response empty: {response_data}",
+            status=self.Status.error,
+        )
+        return text
+
+    if len(response_data["translations"]) > 1:
+        self.log_translation(
+            input_text=text,
+            result=f"more than 1 translation: {response_data['translations']})",
+            status=self.Status.warning,
         )
 
-        dec_text = self.decode_text(
-            text=response_data["translations"][0]["text"],
-        )
+    self.log_translation(
+        input_text=text,
+        result=response_data["translations"][0]["text"],
+        status=self.Status.success,
+    )
 
-        self.cached[text] = dec_text
+    dec_text = self.decode_text(
+        text=response_data["translations"][0]["text"],
+    )
 
-        return dec_text
+    # Replace placeholders with original variables
+    for i, var in enumerate(variables):
+        placeholder = f"PLACEHOLDER_{i}"
+        dec_text = dec_text.replace(placeholder, var)
+
+    self.cached[text] = dec_text
+
+    return dec_text
